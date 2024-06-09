2f54ccd858052400e8ba2f8f97917f6cff791bf8
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-06-09 16:18:50 +0000

    added media processor and view all_products

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,33 @@
+
 from django.contrib import admin
 from .models import Product, Category, Collection
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Collection)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "category",
+        "price",
+        "rating",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name"
+    )
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name"
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Collection, CollectionAdmin)
