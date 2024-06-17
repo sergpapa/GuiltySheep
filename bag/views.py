33cb0a521ba098f5962212d7b6503bae1eb67adf
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-06-17 08:05:15 +0000

    adjust and remove item from bag

--- bag/views.py
+++ bag/views.py
@@ -38,11 +38,45 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 
-
-
 def adjust_bag(request, item_id):
-    pass
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+  
+    if quantity > 0:
+        bag[item_id]['items_by_size'][size] = quantity
+        messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+    else:
+        del bag[item_id]['items_by_size'][size]
+        if not bag[item_id]['items_by_size']:
+            bag.pop(item_id)
+        messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+   
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
-    pass
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        del bag[item_id]['items_by_size'][size]
+        if not bag[item_id]['items_by_size']:
+            bag.pop(item_id)
+        messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+       
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
