833fcfcdd175495b19348dc8d421cb701001707a
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-06-22 17:42:48 +0000

    added collection cards on home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from products.models import Collection
 
 # Create your views here.
 def index(request):
     """ A view to return home url """
+    collections = Collection.objects.all()
+
+    context = {
+        'collections' : collections
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
