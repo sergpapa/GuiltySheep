dca97d6bbe442629f8260531605649a4048b4b9b
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-10-07 10:40:12 +0000

    removed secret key from settings

--- guilty_sheep/settings.py
+++ guilty_sheep/settings.py
@@ -12,9 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7@@5p$+jitb(4%%^1ib6w*9__)c5g#yyi3wz!uq560mb)ezz29'  # need to change with env variable when deloyed os.environ.get('SECRET_KEY', '')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
